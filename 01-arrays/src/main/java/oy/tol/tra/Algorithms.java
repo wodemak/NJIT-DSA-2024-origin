fcdaef3e7159cc52e77d64d0e3f135546d2f1560
wodemak <3249680176@qq.com>
2024-03-11 22:08:42 +0800

    01

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -6,9 +6,7 @@ public class Algorithms {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j].compareTo(array[j + 1]) > 0) {
-                    T temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                    swap(array, j, j + 1);
                 }
             }
         }
@@ -19,11 +17,16 @@ public class Algorithms {
         int i = 0;
         int j = array.length - 1;
         while (i < j) {
-            T temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            swap(array, i, j);
             i++;
             j--;
         }
     }
+
+    private static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
 }
--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -31,13 +31,7 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
-      int i = 0;
-      while (i < grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
+      Algorithms.reverse(grades);
    }
 
    /**
@@ -52,24 +46,7 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
       */
-      int len = grades.length-1;
-      // while (i > 0) {
-      //    if (grades[i] < grades[i-1]) {
-      //       int tmp = grades[i];
-      //       grades[i] = grades[i-1];
-      //       grades[i-1] = tmp;
-      //    }
-      //    i--;
-      // }
-      for(int i = 0; i < len; i++){
-         for(int j = 0; j < len - i; j++){
-            if (grades[j] > grades[j+1]) {        // 相邻元素两两对比
-               int temp = grades[j+1];        // 元素交换
-               grades[j+1] = grades[j];
-               grades[j] = temp;
-           }
-         }
-      }
+      Algorithms.sort(grades);
    }
 
    /**
